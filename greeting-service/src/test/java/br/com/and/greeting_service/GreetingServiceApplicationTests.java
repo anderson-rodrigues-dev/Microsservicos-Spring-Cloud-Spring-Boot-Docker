6af6aed07b624e75ad3188322662c307c649b9eb
Anderson Rodrigues <and.rt@hotmail.com>
2024-12-14 21:14:17 -0300

    Initial Config

---
+++ greeting-service/src/test/java/br/com/and/greeting_service/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.and.greeting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
