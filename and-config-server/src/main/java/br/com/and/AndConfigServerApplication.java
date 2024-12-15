072533fd3320795e04458ae418bcd3bb0b0045b7
Anderson Rodrigues <and.rt@hotmail.com>
2024-12-15 17:55:37 -0300

    Section Conclusion - Spring Cloud Configuration and Spring Boot Actuator

--- and-config-server/src/main/java/br/com/and/AndConfigServerApplication.java
+++ and-config-server/src/main/java/br/com/and/AndConfigServerApplication.java
@@ -2,8 +2,10 @@ package br.com.and;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AndConfigServerApplication {
 
 	public static void main(String[] args) {
